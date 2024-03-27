ac29e496963d01ef78b580fc64826bb848011656
Martina Scheffler <martina.scheffler00@gmail.com>
2024-03-27 10:49:44 +0100

    Use matrix generation and correctness check

--- Homework1/simple_transpose.cpp
+++ Homework1/simple_transpose.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <cmath>
+
+#include "matrix_generation.h"
+#include "check_correctness.h"
+
 using namespace std;
 
 int* simple_transpose(int *A, int N){
@@ -15,21 +20,23 @@ int* simple_transpose(int *A, int N){
 
 
 int main (int argc, char* argv[]){
+	// check if the matrix size was provided
+	if (argc < 2){
+		throw runtime_error("Please enter an integer N as argument to generate a matrix of size 2^N x 2^N.");
+	}
+
+	int size = pow(2, atoi(argv[1]));
+
 	// call matrix generation with command line argument and receive matrix back
-
-
-	// define identity matrix in the meantime
-	int A[4 * 4] = {1, 2, 3, 4,
-					5, 6, 7, 8,
-					9, 10, 11, 12,
-					13, 14, 15, 16};
+	int* A = generate_continous_matrix(size);
 
 	// transpose matrix
-	int* A_t = simple_transpose(A, 4);
+	int* A_t = simple_transpose(A, size);
 
-
-	// check for correct transpose
-
+	// check for correctness using Eigen
+	if (!check_transpose_correctness(A, A_t, size)){
+		throw runtime_error("Checking for correctness failed.");
+	}
 
 	// display result
 	for (int i=0; i<4; i++){

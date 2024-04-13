4eac8589083c977d48c380df4f9e199581b24854
Martina Scheffler <martina.scheffler00@gmail.com>
2024-04-13 14:47:28 +0200

    Add sbatch script

---
+++ Homework1/compile_flag_analysis_sbatch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=compile_flag_analysis
+#SBATCH --output=cfa_%j.out
+#SBATCH --error=cfa_%j.err
+#SBATCH --partition=edu5
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:0
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+./compile_flag_analysis

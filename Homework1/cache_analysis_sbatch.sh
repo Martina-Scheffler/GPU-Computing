1cbec7c1c8f83db7e41e8592fd879ff5d3e66a52
Martina Scheffler <martina.scheffler00@gmail.com>
2024-04-18 18:50:04 +0200

    Add missing gres tag

--- Homework1/cache_analysis_sbatch.sh
+++ Homework1/cache_analysis_sbatch.sh
@@ -4,6 +4,7 @@
 #SBATCH --error=cache_%j.err
 #SBATCH --partition=edu5
 #SBATCH --nodes=1
+#SBATCH --gres=gpu:0
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 
--- Homework1/compile_flag_analysis_sbatch.sh
+++ Homework1/compile_flag_analysis_sbatch.sh
@@ -3,6 +3,7 @@
 #SBATCH --output=cfa_%j.out
 #SBATCH --error=cfa_%j.err
 #SBATCH --partition=edu5
+#SBATCH --gres=gpu:0
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1

2d9eb5b1bd1b906c8c212ba7963dbbb7b359bfda
Martina Scheffler <martina.scheffler00@gmail.com>
2024-04-18 16:47:16 +0200

    Increase readability of plot in paper

--- Homework1/visualization/plot.py
+++ Homework1/visualization/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-plt.rcParams.update({'font.size': 22})
+plt.rcParams.update({'font.size': 26})
 
 df_simple_00 = pd.read_csv("output/simple_transpose_00.csv", header=None, sep=';')
 df_simple_01 = pd.read_csv("output/simple_transpose_01.csv", header=None, sep=';')
@@ -25,19 +25,18 @@ df_block_03['mean'] = df_block_03.mean(axis=1)
 
 plt.figure(figsize=(20, 10))
 
-plt.plot(range(1, 13), df_simple_00['mean'], 'b--', label='Simple Transpose -O0')
-plt.plot(range(1, 13), df_simple_01['mean'], 'r--', label='Simple Transpose -O1')
-plt.plot(range(1, 13), df_simple_02['mean'], 'g--', label='Simple Transpose -O2')
-plt.plot(range(1, 13), df_simple_03['mean'], 'c--', label='Simple Transpose -O3')
+plt.plot(range(1, 13), df_simple_00['mean'], color='tab:blue', linestyle='--', linewidth=4, label='Simple Transpose -O0')
+plt.plot(range(1, 13), df_simple_01['mean'], color='tab:red', linestyle='--', linewidth=4, label='Simple Transpose -O1')
+plt.plot(range(1, 13), df_simple_02['mean'], color='tab:green', linestyle='--', linewidth=4, label='Simple Transpose -O2')
+plt.plot(range(1, 13), df_simple_03['mean'], color='tab:orange', linestyle='--', linewidth=4, label='Simple Transpose -O3')
 
-plt.plot(range(1, 13), df_block_00['mean'], 'b-', label='Block Transpose -O0')
-plt.plot(range(1, 13), df_block_01['mean'], 'r-', label='Block Transpose -O1')
-plt.plot(range(1, 13), df_block_02['mean'], 'g-', label='Block Transpose -O2')
-plt.plot(range(1, 13), df_block_03['mean'], 'c-', label='Block Transpose -O3')
-
+plt.plot(range(1, 13), df_block_00['mean'], color='tab:blue', linestyle='-', linewidth=4, label='Block Transpose -O0')
+plt.plot(range(1, 13), df_block_01['mean'], color='tab:red', linestyle='-', linewidth=4, label='Block Transpose -O1')
+plt.plot(range(1, 13), df_block_02['mean'], color='tab:green', linestyle='-', linewidth=4, label='Block Transpose -O2')
+plt.plot(range(1, 13), df_block_03['mean'], color='tab:orange', linestyle='-', linewidth=4, label='Block Transpose -O3')
 
 plt.xlabel("Matrix Dimension: $2^{N} \\times 2^{N}$")
 plt.ylabel('Execution Time [ms]')
 plt.legend()
-plt.savefig('./visualization/execution_time_comparison.png')
+plt.savefig('./visualization/execution_time_comparison.png', dpi=600)
 plt.show()

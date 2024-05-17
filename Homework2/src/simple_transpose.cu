982abc12334c25ad9b461b44be88cb113268a14e
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-17 18:07:03 +0200

    Add logic to simple transpose

--- Homework2/src/simple_transpose.cu
+++ Homework2/src/simple_transpose.cu
@@ -1,5 +1,11 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
+
+#include "../include/matrix_generation.h"
+
+using namespace std;
 
 int TILE_DIMENSION = 4;
 int BLOCK_ROWS = 1;
@@ -15,12 +21,62 @@ __global__ void simpleTransposeKernel(int* A, int* A_T){
 }
 
 
-int main(void){
-    int* A, A_T;
-    N = 4;
-	dim3 nBlocks = (N / TILE_DIMENSION, N / TILE_DIMENSION, 1);
-    dim3 nThreads = (TILE_DIMENSION, BLOCK_ROWS, 1);
-	simpleTransposeKernel<<<nBlocks, nThreads>>>(A, A_T);
-	cudaDeviceSynchronize();
-	return 0;
+int main(int argc, char* argv[]){
+    // check if the matrix size was provided
+	if (argc < 2){
+		throw runtime_error("Please enter an integer N as argument to generate a matrix of size 2^N x 2^N.");
+	}
+    
+    if (atoi(argv[1]) == 0){
+        // use zero for something later
+    }
+    else {
+        int size = pow(2, atoi(argv[1]));
+        int N = size * size;
+
+		// call matrix generation with command line argument and receive matrix back
+		int* A = generate_continous_matrix(size);
+
+        // allocate memory on device
+        int *dev_A, *dev_A_t;
+
+        checkCudaErrors( cudaMalloc(&dev_A, N * sizeof(float)) );
+        checkCudaErrors( cudaMalloc(&dev_A_t, N * sizeof(float)) );
+
+        // copy matrix to device
+        cudaMemcpy(dev_A, A, N * sizeof(float), cudaMemcpyHostToDevice);
+
+        // start CUDA timer
+
+        // determine kernel dimensions
+        dim3 nBlocks = (size / TILE_DIMENSION, size / TILE_DIMENSION, 1);
+        dim3 nThreads = (TILE_DIMENSION, BLOCK_ROWS, 1);
+
+        // run kernel
+        simpleTransposeKernel<<<nBlocks, nThreads>>>(A, A_t);
+
+        // synchronize
+        checkCudaErrors( cudaDeviceSynchronize() );
+
+        // stop CUDA timer
+
+        // allocate memory on host
+        int* A_t = (int*) malloc(N * sizeof(float));
+
+        // copy back
+        cudaMemcpy(dev_A_t, A_t, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+        // display result
+        
+        // free memory on device
+        checkCudaErrors( cudaFree(dev_A) );
+        checkCudaErrors( cudaFree(dev_A_t) );
+
+        // free memory on host
+        free(A);
+        free(A_t);
+
+        return 0;
+    }
+
 }

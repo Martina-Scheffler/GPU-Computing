6d01d4d302e073e7de27682bac5f87113484a76e
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-17 17:31:10 +0200

    Add missing commands

--- Homework2/transpose_sbatch.sh
+++ Homework2/transpose_sbatch.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#SBATCH --job-name=transpose
+#SBATCH --output=tp_%j.out
+#SBATCH --error=tp_%j.err
+#SBATCH --partition=edu5
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
 
 # load cuda
 module load cuda

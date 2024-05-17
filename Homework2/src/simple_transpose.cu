2a8adf0c7747ac773b12d0319870909c5bb435c9
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-17 19:20:02 +0200

    Simple test

--- Homework2/src/simple_transpose.cu
+++ Homework2/src/simple_transpose.cu
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]){
 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; i++){
-                cout << A[i*size + j] << "\t";
+                cout << i*size + j << "\t";
             }
             cout << "\n";
         }

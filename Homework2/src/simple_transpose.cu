2e95a06f6645d41b9caabf99c7df3a0f5806af86
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-18 16:40:19 +0200

    Fix sizeof datatype

--- Homework2/src/simple_transpose.cu
+++ Homework2/src/simple_transpose.cu
@@ -12,13 +12,13 @@ using namespace std;
 #define TILE_DIMENSION 4
 #define BLOCK_ROWS 1
 
-__global__ void simpleTransposeKernel(int* A, int* A_T){
+__global__ void simpleTransposeKernel(int* A, int* A_t){
 	int x = blockIdx.x * TILE_DIMENSION + threadIdx.x;
 	int y = blockIdx.y * TILE_DIMENSION + threadIdx.y;
     int width = gridDim.x * TILE_DIMENSION;
 
     for(int i=0; i<TILE_DIMENSION; i+=BLOCK_ROWS){
-        A_T[x * width + (y + i)] = A[(y + i) * width + x];
+        A_t[x * width + (y + i)] = A[(y + i) * width + x];
     }
 }
 
@@ -41,21 +41,21 @@ int main(int argc, char* argv[]){
 		// call matrix generation with command line argument and receive matrix back
 		int* A = generate_continous_matrix(size);
 
-        // for (int i=0; i<size; i++){
-        //     for (int j=0; j<size; i++){
-        //         cout << i*size + j << "\t";
-        //     }
-        //     cout << "\n";
-        // }
+        for (int i=0; i<size; i++){
+            for (int j=0; j<size; i++){
+                cout << i*size + j << "\t";
+            }
+            cout << "\n";
+        }
 
         // allocate memory on device
         int *dev_A, *dev_A_t;
 
-        cudaMalloc(&dev_A, N * sizeof(float));
-        cudaMalloc(&dev_A_t, N * sizeof(float));
+        cudaMalloc(&dev_A, N * sizeof(int));
+        cudaMalloc(&dev_A_t, N * sizeof(int));
 
         // copy matrix to device
-        cudaMemcpy(dev_A, A, N * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_A, A, N * sizeof(int), cudaMemcpyHostToDevice);
 
         // start CUDA timer
 
@@ -72,18 +72,18 @@ int main(int argc, char* argv[]){
         // stop CUDA timer
 
         // allocate memory on host
-        int* A_t = (int*) malloc(N * sizeof(float));
+        int* A_t = (int*) malloc(N * sizeof(int));
 
         // copy back
-        cudaMemcpy(dev_A_t, A_t, N * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(dev_A_t, A_t, N * sizeof(int), cudaMemcpyDeviceToHost);
 
         // display result
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; i++){
-                cout << A_t[i*size + j] << "\t";
-            }
-            cout << "\n";
-        }
+        // for (int i=0; i<size; i++){
+        //     for (int j=0; j<size; i++){
+        //         cout << A_t[i*size + j] << "\t";
+        //     }
+        //     cout << "\n";
+        // }
         
         // free memory on device
         cudaFree(dev_A);

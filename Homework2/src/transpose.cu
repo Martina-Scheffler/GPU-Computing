66e726d8aaaa73aa674e450c83b38c93cc38c72a
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-21 17:18:44 +0200

    Add variable tile dimension and block rows as input args

--- Homework2/src/transpose.cu
+++ Homework2/src/transpose.cu
@@ -9,10 +9,10 @@
 
 using namespace std;
 
-#define TILE_DIMENSION 32
-#define BLOCK_ROWS 8
-
 int strategy = 0;
+int tileDimension = 4;
+int blockRows = 1;
+
 
 __global__ void warm_up_gpu(){
     unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -21,59 +21,59 @@ __global__ void warm_up_gpu(){
     ib += ia + tid; 
 }
 
-__global__ void transposeSimple(int* A, int* A_T){
-	int x = blockIdx.x * TILE_DIMENSION + threadIdx.x;
-	int y = blockIdx.y * TILE_DIMENSION + threadIdx.y;
-    int width = gridDim.x * TILE_DIMENSION;
+__global__ void transposeSimple(int* A, int* A_T, int tileDimension, int blockRows){
+	int x = blockIdx.x *  + threadIdx.x;
+	int y = blockIdx.y * tileDimension + threadIdx.y;
+    int width = gridDim.x * tileDimension;
 
-    for(int i=0; i<TILE_DIMENSION; i+=BLOCK_ROWS){
+    for(int i=0; i<tileDimension; i+=blockRows){
         A_T[x * width + (y + i)] = A[(y + i) * width + x];
     }
 }
 
-__global__ void transposeCoalesced(int *A, int *A_T){
-    __shared__ int tile[TILE_DIMENSION][TILE_DIMENSION + 1];  // +1 in y to avoid bank conflicts
+__global__ void transposeCoalesced(int *A, int *A_T, int tileDimension, int blockRows){
+    __shared__ int tile[tileDimension][tileDimension + 1];  // +1 in y to avoid bank conflicts
 
-    int x = blockIdx.x * TILE_DIMENSION + threadIdx.x;
-    int y = blockIdx.y * TILE_DIMENSION + threadIdx.y;
-    int width = gridDim.x * TILE_DIMENSION;
+    int x = blockIdx.x * tileDimension + threadIdx.x;
+    int y = blockIdx.y * tileDimension + threadIdx.y;
+    int width = gridDim.x * tileDimension;
 
-    for (int i=0; i<TILE_DIMENSION; i+=BLOCK_ROWS){
+    for (int i=0; i<tileDimension; i+=blockRows){
         tile[threadIdx.y + i][threadIdx.x] = A[(y + i) * width + x];
     }
         
     __syncthreads();
 
-    x = blockIdx.y * TILE_DIMENSION + threadIdx.x;  // transpose block offset
-    y = blockIdx.x * TILE_DIMENSION + threadIdx.y;
+    x = blockIdx.y * tileDimension + threadIdx.x;  // transpose block offset
+    y = blockIdx.x * tileDimension + threadIdx.y;
 
-    for (int i=0; i<TILE_DIMENSION; i+=BLOCK_ROWS){
+    for (int i=0; i<tileDimension; i+=blockRows){
         A_T[(y + i) * width + x] = tile[threadIdx.x][threadIdx.y + i];
     }
 }
 
-__global__ void transposeDiagonal(int *A, int *A_T){
-    __shared__ int tile[TILE_DIMENSION][TILE_DIMENSION + 1];
+__global__ void transposeDiagonal(int *A, int *A_T, int tileDimension, int blockRows){
+    __shared__ int tile[tileDimension][tileDimension + 1];
 
     // diagonal reordering
     int blockIdx_y = blockIdx.x;
     int blockIdx_x = (blockIdx.x + blockIdx.y) % gridDim.x;
 
-    int x = blockIdx_x * TILE_DIMENSION + threadIdx.x;
-    int y = blockIdx_y * TILE_DIMENSION + threadIdx.y;
-    int width = gridDim.x * TILE_DIMENSION;
+    int x = blockIdx_x * tileDimension + threadIdx.x;
+    int y = blockIdx_y * tileDimension + threadIdx.y;
+    int width = gridDim.x * tileDimension;
 
 
-    for (int i=0; i<TILE_DIMENSION; i+=BLOCK_ROWS){
+    for (int i=0; i<tileDimension; i+=blockRows){
         tile[threadIdx.y + i][threadIdx.x] = A[(y + i) * width + x];
     }
         
     __syncthreads();
 
-    x = blockIdx_y * TILE_DIMENSION + threadIdx.x;  
-    y = blockIdx_x * TILE_DIMENSION + threadIdx.y;
+    x = blockIdx_y * tileDimension + threadIdx.x;  
+    y = blockIdx_x * tileDimension + threadIdx.y;
 
-    for (int i=0; i<TILE_DIMENSION; i+=BLOCK_ROWS){
+    for (int i=0; i<tileDimension; i+=blockRows){
         A_T[(y + i) * width + x] = tile[threadIdx.x][threadIdx.y + i];
     }
 }
@@ -88,9 +88,14 @@ int main(int argc, char* argv[]){
         strategy = atoi(argv[2]);  // Strategy: 0 = Simple, 1 = Coalesced, 2 = Diagonal
         printf("Strategy %d\n", strategy);
     }
+    if (argc >= 5){
+        tileDimension = atoi(argv[3]);
+        blockRows = atoi(argv[4]);
+    }
     
     if (atoi(argv[1]) == 0){
-        // use zero for something later
+        // use zero for analyzing the effective bandwidth using different TILE_DIMENSION and BLOCK_ROWS values
+
     }
     else {
         int size = pow(2, atoi(argv[1]));
@@ -101,23 +106,23 @@ int main(int argc, char* argv[]){
 		// call matrix generation with command line argument and receive matrix back
 		int* A = generate_continous_matrix(size);
 
-        // for (int i=0; i<size; i++){
-        //     for (int j=0; j<size; j++){
-        //         cout << A[i * size + j] << "\t";
-        //     }
-        //     cout << endl;
-        // }
-        // cout << endl;
+        for (int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                cout << A[i * size + j] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
 
         // allocate memory on host
         int* A_T = (int*) malloc(N * sizeof(int));
 
         // determine kernel dimensions
-        dim3 nBlocks (size / TILE_DIMENSION, size / TILE_DIMENSION, 1);
-        dim3 nThreads (TILE_DIMENSION, BLOCK_ROWS, 1);
+        dim3 nBlocks (size / tileDimension, size / tileDimension, 1);
+        dim3 nThreads (tileDimension, blockRows, 1);
 
-        cout << "Blocks: " << size / TILE_DIMENSION << endl;
-        cout << "Threads: " << TILE_DIMENSION << " " << BLOCK_ROWS << endl;
+        cout << "Blocks: " << size / tileDimension << " " << size / tileDimension << endl;
+        cout << "Threads: " << tileDimension << " " << blockRows << endl;
 
         // allocate memory on device
         int *dev_A, *dev_A_T;
@@ -141,13 +146,13 @@ int main(int argc, char* argv[]){
             
         // run kernel
         if (strategy == 0){  // Simple kernel
-            transposeSimple<<<nBlocks, nThreads>>>(dev_A, dev_A_T);
+            transposeSimple<<<nBlocks, nThreads>>>(dev_A, dev_A_T, tileDimension, blockRows);
         }
         else if (strategy == 1){  // Coalesced kernel
-            transposeCoalesced<<<nBlocks, nThreads>>>(dev_A, dev_A_T);
+            transposeCoalesced<<<nBlocks, nThreads>>>(dev_A, dev_A_T, tileDimension, blockRows);
         }
         else if (strategy == 2){  // Diagonal kernel
-            transposeDiagonal<<<nBlocks, nThreads>>>(dev_A, dev_A_T);
+            transposeDiagonal<<<nBlocks, nThreads>>>(dev_A, dev_A_T, tileDimension, blockRows);
         }
         else {
             throw runtime_error("Please choose 0, 1 or 2 for the strategy.");
@@ -169,13 +174,13 @@ int main(int argc, char* argv[]){
         // copy back to host
         cudaMemcpy(A_T, dev_A_T, N * sizeof(int), cudaMemcpyDeviceToHost);
         
-        // // display result
-        // for (int i=0; i<size; i++){
-        //     for (int j=0; j<size; j++){
-        //         cout << A_T[i*size + j] << "\t";
-        //     }
-        //     cout << endl;
-        // }
+        // display result
+        for (int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                cout << A_T[i*size + j] << "\t";
+            }
+            cout << endl;
+        }
 
         // Free timer events
 	    cudaEventDestroy(start);

f3142d0a08dadae499163f143d4e2e64593b0aa8
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-28 17:29:00 +0200

    Change cublas call

--- Homework2/src/transpose.cu
+++ Homework2/src/transpose.cu
@@ -111,7 +111,7 @@ bool checkCorrectness(int* A, int* A_T, int size){
     // }
 
     cublasCreate(&handle);
-    cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, size, size, &alpha, A_copy, size, &beta, A_copy, size, res, size);
+    cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, size, size, &alpha, A_copy, size, &beta, A_copy, size, A, size);
     cublasDestroy(handle);
 
     bool correct = true;
@@ -120,7 +120,7 @@ bool checkCorrectness(int* A, int* A_T, int size){
     // display cublas result
     for (int i=0; i<size; i++){
         for (int j=0; j<size; j++){
-                cout << res[i * size + j] << "\t";
+                cout << A[i * size + j] << "\t";
         }
         cout << endl;
     }

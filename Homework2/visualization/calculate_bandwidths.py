7fe4935a1f649d8d4bf9158b4561416279b13819
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-28 18:24:45 +0200

    Calculate BW using new layout after correctness check

--- Homework2/visualization/calculate_bandwidths.py
+++ Homework2/visualization/calculate_bandwidths.py
@@ -3,33 +3,48 @@ import pandas as pd
 import numpy as np
 plt.rcParams.update({'font.size': 26})
 
+
+def print_bandwidths(df):
+    # calculate bandwidth
+    tile_dimensions = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    block_rows = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+    size = 2
+    td_id = 0
+    skipped_already = False
+    max_bandwidth = 0
+
+    for row in range(df.shape[0]):
+        if not skipped_already and np.isnan(df.iat[row, 0]):
+            size *= 2
+            td_id = 0
+            skipped_already = True
+        elif not np.isnan(df.iat[row, 0]):
+            skipped_already = False
+            print(f'Size: {size}')
+            col = 0
+            while col < df.shape[1] and not np.isnan(df.iat[row, col]):
+                # formula here
+                effective_bandwidth = ((size * size * 2 * 4) / 10**9) / (df.iat[row, col] * 10**(-3))
+                if(effective_bandwidth > max_bandwidth):
+                    max_bandwidth = effective_bandwidth
+                print(f'TD: {tile_dimensions[td_id]}, BR: {block_rows[col]}, EB: {effective_bandwidth}')
+                col += 1
+            print()
+            td_id += 1
+    
+    print(f'Max. EBW: {max_bandwidth}')
+    return max_bandwidth
+
 # read in data from files
 df_simple = pd.read_csv("output/analyze_bandwidth_0.csv", header=None, sep=';', names=range(11), skip_blank_lines=False)
-df_coalesced = pd.read_csv("output/analyze_bandwidth_0.csv", header=None, sep=';', names=range(11))
-df_diagonal = pd.read_csv("output/analyze_bandwidth_0.csv", header=None, sep=';', names=range(11))
+df_coalesced = pd.read_csv("output/analyze_bandwidth_1.csv", header=None, sep=';', names=range(11), skip_blank_lines=False)
+df_diagonal = pd.read_csv("output/analyze_bandwidth_2.csv", header=None, sep=';', names=range(11), skip_blank_lines=False)
 
-# calculate bandwidth
-tile_dimensions = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-block_rows = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
-size = 2
-td_id = 0
-
-for row in range(df_simple.shape[0]):
-    if np.isnan(df_simple.iat[row, 0]):
-        size *= 2
-        td_id = 0
-    else:
-        print(f'Size: {size}')
-        col = 0
-        while  col < df_simple.shape[1] and not np.isnan(df_simple.iat[row, col]):
-            # formula here
-            effective_bandwidth = ((size * size * 2 * 4) / 10**9) / (df_simple.iat[row, col] * 10**(-3))
-            print(f'TD: {tile_dimensions[td_id]}, BR: {block_rows[col]}, EB: {effective_bandwidth}')
-            col += 1
-        print()
-        td_id += 1
-
+max_simple = print_bandwidths(df=df_simple)
+max_coalesced = print_bandwidths(df=df_coalesced)
+max_diagonal = print_bandwidths(df=df_diagonal)
 
 
 

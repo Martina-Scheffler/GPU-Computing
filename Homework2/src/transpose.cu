48d32f0167719443dd0783852322e71f571d9d2c
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-28 16:54:32 +0200

    Correct datatypes

--- Homework2/src/transpose.cu
+++ Homework2/src/transpose.cu
@@ -97,7 +97,7 @@ bool checkCorrectness(int* A, int* A_T, int size){
 
     for (int i=0; i<size; i++){
         for (int j=0; j<size; j++){
-            if (A_T[i * size + j] != res[i * size + j]) {
+            if (A_T[i * size + j] != (int) res[i * size + j]) {
                 return false;
             }
         }

c91c03973a68119ecdfc874b338962815eee4a80
Martina Scheffler <martina.scheffler00@gmail.com>
2024-05-28 17:57:50 +0200

    Remove detailed print

--- Homework2/src/transpose.cu
+++ Homework2/src/transpose.cu
@@ -123,7 +123,7 @@ bool checkCorrectness(int* A, int* A_T, int size){
     for (int i=0; i<size; i++){
         for (int j=0; j<size; j++){
             if (A_T[i * size + j] != (int) res[i * size + j]) {
-                printf("%d != %d\n", A_T[i * size + j], res[i * size + j]);
+                // printf("%d != %d\n", A_T[i * size + j], res[i * size + j]);
                 correct = false;
             }
         }

c524703c1910115b332d935a2c03b4b330cbd3d3
Martina Scheffler <martina.scheffler00@gmail.com>
2024-07-08 17:22:20 +0200

    Print values

--- FinalProject/src/transpose.cu
+++ FinalProject/src/transpose.cu
@@ -92,8 +92,8 @@ void transpose_cuSparse_CSR(string file){
     cudaMemcpy(row_offsets_tp, dev_tp_col_offsets, (columns + 1) * sizeof(int), cudaMemcpyDeviceToHost);
     cudaMemcpy(values_tp, dev_tp_values, nnz * sizeof(float), cudaMemcpyDeviceToHost);
 
-    for (int i=0; i<columns+1; i++){
-        printf("%d\n", row_offsets_tp[i]);
+    for (int i=0; i<nnz; i++){
+        printf("%f\n", values_tp[i]);
     }
 
     // save transposed matrix to file

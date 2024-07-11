138a9fd845e4b80b293b2fe7390c87c8305f021b
Martina Scheffler <martina.scheffler00@gmail.com>
2024-07-11 20:54:10 +0200

    Fix err file path

--- FinalProject/transpose_all_sbatch.sh
+++ FinalProject/transpose_all_sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=transpose_sparse_all
 #SBATCH --output=tps_all_%j.out
-#SBATCH --error=tps_%j.err
+#SBATCH --error=tps_all_%j.err
 #SBATCH --partition=edu5
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
